b54ce3b6033a978ade82bb23fb66f26e382347cb
HyangKeun Choi <gidrms99@naver.com>
2022-04-25 21:55:41 +0900

    refactor : 클래스 분리

--- src/main/java/racingcar/view/GameRunner.java
+++ src/main/java/racingcar/view/GameRunner.java
@@ -10,13 +10,22 @@ public class GameRunner {
     RacingGame racingGame = new RacingGame();
 
     public void startGame() {
-        System.out.println(ConstantMessage.GAME_START_MESSAGE);
-        Cars cars = racingGame.play(Console.readLine());
-        System.out.println(ConstantMessage.GAME_TRIAL_COUNT);
-        int trial = Integer.parseInt(Console.readLine());
+        Cars cars = inputCarNames();
+        int trial = inputTrialCount();
         Cars resultCars = racingGame.attemptRacingCar(cars, trial);
         racingGame.drawCarMove(resultCars);
         racingGame.printWinnerCar(resultCars);
     }
 
+    private Cars inputCarNames() {
+        System.out.println(ConstantMessage.GAME_START_MESSAGE);
+        Cars cars = racingGame.play(Console.readLine());
+        return cars;
+    }
+
+    private int inputTrialCount() {
+        System.out.println(ConstantMessage.GAME_TRIAL_COUNT);
+        int trial = Integer.parseInt(Console.readLine());
+        return trial;
+    }
 }
